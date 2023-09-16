e4f41572286aa35624a3a3a5e89fe75bc703b12a
snopiro <khoochris13@gmail.com>
2023-09-16 18:41:28 -0500

    updated scripts in package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello World! This is my first React App. I am so excited to learn React.
         </p>
         <a
           className="App-link"
