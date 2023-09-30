6642fb35d9d94cbef2cffa246c5bb98102d5ba03
dangle124 <khoale12420@gmail.com>
2023-09-29 22:46:42 -0500

    pass data from MyAccount to EditMyAccount page

--- profile/src/EditMyAccount.js
+++ profile/src/EditMyAccount.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState,  } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 import EditAccountInfo from './components/EditMyAccountInfo.js'
@@ -6,29 +7,18 @@ import Axios from 'axios';
 
 
 const EditMyAccount = () => {
-    // create holder for values and function to update values
-    const [accInfo, setAccInfo] = useState([{
-        name: 'Puppy Cute',
-        email: 'puppy.cute@animal.com',
-        phone: '(999)999-999',
-        about: 'I am the cutest puppy in the world.',
-        subject: 'Math',
-    }]);
-
-    // // retrieve data from db
-    // useEffect(() => {
-    //     // call GET api 
-    //     Axios.get('http://localhost:3006/accountInfo/')
-
-    //     // get result and store data
-    //     .then(response => {
-    //         setAccInfo(response.data);
-    //         console.log(accInfo)
-    //     })
-
-    //     // handle error
-    //     .catch(err => console.log(err))
-    // }, [])
+    // get data passed from MyAccount page
+    const location = useLocation();
+    const {fromMyAccount} = location.state;
+    
+    // get data from MyAccount and store as initial state
+    const [accInfo, setAccInfo] = useState({
+        name: fromMyAccount.data.name,
+        email: fromMyAccount.data.email, 
+        phone: fromMyAccount.data.phone,
+        about: fromMyAccount.data.about,
+        subject: fromMyAccount.data.subject
+    });
 
     // update values when any changes
     const handleChange = (e) => {
@@ -49,7 +39,6 @@ const EditMyAccount = () => {
         .then(response => console.log(response.data))
         .catch(err => console.log(err));
     }
-
 
     return (
         <div>
--- profile/src/components/MyAccountInfo.js
+++ profile/src/components/MyAccountInfo.js
@@ -3,6 +3,13 @@ import { TextField, Box, Button } from "@mui/material/";
 import { Link } from "react-router-dom";
 
 export default function AccountInfo(props) {
+  const data = {
+    name: props.accInfo.name,
+    email: props.accInfo.email, 
+    phone: props.accInfo.phone,
+    about: props.accInfo.about,
+    subject: props.accInfo.subject
+  }
   return (
     <Box
       component="form"
@@ -16,45 +23,45 @@ export default function AccountInfo(props) {
       <TextField
         id="name"
         label="First and Last Name"
-        defaultValue={props.accInfo.name}
+        defaultValue={data.name}
         InputProps={{ readOnly: true }}
       />
       <TextField
         id="email"
         label="Email Address"
-        defaultValue={props.accInfo.email}
+        defaultValue={data.email}
         InputProps={{ readOnly: true }}
       />
       <TextField
         id="phone"
         label="Phone Number"
-        defaultValue={props.accInfo.phone}
+        defaultValue={data.phone}
         InputProps={{ readOnly: true }}
       />
       <TextField
         id="about"
         label="About Me"
-        defaultValue={props.accInfo.about}
+        defaultValue={data.about}
         multiline
         InputProps={{ readOnly: true }}
       />
       <TextField
         id="subjects"
         label="Subject List"
-        defaultValue={props.accInfo.subject}
+        defaultValue={data.subject}
         InputProps={{ readOnly: true }}
       />
       <br />
 
-      <Button
-        component={Link}
-        to="/EditMyAccount"
-        variant="contained"
-        size="large"
-        style={{ marginBottom: "8px" }}
-      >
-        Edit
-      </Button>
+      <Link to="/EditMyAccount" state={{ fromMyAccount: {data}}} >
+        <Button
+          variant="contained"
+          size="large"
+          style={{ marginBottom: "8px" }}
+        >
+          Edit
+        </Button>
+      </Link>
     </Box>
   );
 }
