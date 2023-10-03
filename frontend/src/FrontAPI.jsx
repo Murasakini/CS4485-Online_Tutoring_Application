a74b3a9d22082f49fab361f3831e77b2d0c62575
kcao2 <104512360+kcao2@users.noreply.github.com>
2023-10-03 12:12:18 -0500

    Add files via upload

--- frontend/src/FrontAPI.jsx
+++ frontend/src/FrontAPI.jsx
@@ -9,6 +9,8 @@ const axiosInstance = axios.create({
 
 // api functions
 const FrontAPI = {
+
+  // send sign in info for checking
   signIn: async (formData) => {
     try {
       const response = await axiosInstance.post('/api/signin', { 
@@ -22,7 +24,8 @@ const FrontAPI = {
       throw error;
     }
   },
-  
+
+  // send sign up info
   signUp: async (formData) => {
     try {
       const response = await axiosInstance.post('/api/signup', { 
@@ -48,6 +51,7 @@ const FrontAPI = {
     }
   },
 
+  //get tutors of the chosen subject
   fetchTutors: async (subject) => {
     try {
       const response = await axiosInstance.get('/api/tutors', {
@@ -61,6 +65,7 @@ const FrontAPI = {
     }
   },
 
+  // get available subjects
   fetchSubjects: async () => {
     try {
       const response = await axiosInstance.get('/api/subjects');
@@ -75,6 +80,35 @@ const FrontAPI = {
     }
   },
 
+  // create a new appointment
+  createAppointment: async (formData) => {
+    try {
+      const response = await axiosInstance.post('/api/appointments', {
+        subject: formData.subject,
+        tutor: formData.tutor,
+        timeSlot: formData.timeSlot,
+        // Add other appointment-related data here
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },  
+
+  // get timeslot of the subject, tutor combination
+  fetchTimeSlots: async (tutor, subject) => {
+    try {
+      const response = await axiosInstance.get('/api/timeslots', {
+        params: {
+          tutor: tutor,
+          subject: subject,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
 };
 
 export default FrontAPI;
