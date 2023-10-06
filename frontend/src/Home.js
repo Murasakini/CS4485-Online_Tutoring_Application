cdce6f67490bf9cb12a17dd514cf367ca501ff69
dangle124 <khoale12420@gmail.com>
2023-10-05 21:32:40 -0500

    replace all files in frontend folder by files currently developing

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import MenuList from "./components/MenuList";
+
+const Home = () => {
+    return (
+        <div>
+            <Header title="HOME"/>
+            <MenuList />
+        </div>
+    );
+}
+ 
+export default Home;
