678c1dc50b6c6a36ab8c63200629c4b6247a0937
snopiro <khoochris13@gmail.com>
2023-10-06 03:38:25 -0500

    version setup and some endpoint work

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,57 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flask import Blueprint, request
+import yaml
+
+#####################
+# Global Variables and Setup
+#####################
+
+version = Blueprint('v1', __name__, url_prefix='/api/v1')
+
+with open("vault/credentials.yml", "r") as ymlfile:
+    creds = yaml.safe_load(ymlfile)
+
+username = creds["database"]["username"]
+password = creds["database"]["password"]
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@online-tutoring-application.ccm0nvuvbmz8.us-east-2.rds.amazonaws.com:3306/ota_db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
 
-with app.app_context():
-    connection = db.engine.connect()
-    try:
-        result = connection.execute(text("SELECT * FROM users"))
-        print("Connection successful:", result.fetchone())
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        connection.close()
+USER_FIELDS = {"first_name", "last_name", "netID", "email", "phone_num", "password"}
+TUTOR_FIELDS = USER_FIELDS.union({"criminal"})
 
+
+#####################
+# Helper Functions
+#####################
+
+def validate_fields(data, expected_fields):
+    return set(data.keys()) == expected_fields
+
+def insert_user(data):
+    sql = text("""
+        INSERT INTO users (first_name, last_name, netID, email, phone_num, password) 
+        VALUES (:first_name, :last_name, :netID, :email, :phone_num, :password)
+    """)
+    db.session.execute(sql, data)
+    db.session.commit()
+
+def insert_tutor(data):
+    sql = text("""
+        INSERT INTO tutors (first_name, last_name, netID, email, phone_num, password, criminal) 
+        VALUES (:first_name, :last_name, :netID, :email, :phone_num, :password, :criminal)
+    """)
+    db.session.execute(sql, data)
+    db.session.commit()
+
+
+#####################
+# Routes
+#####################
 
 @app.route("/", methods=["GET"])
 def index():
@@ -28,7 +59,58 @@ def index():
         "message": "Hello World!"
     }
 
+@version.route("/signup/user", methods=["POST"])
+def signup_user():
+    data = request.get_json()
 
+    if not validate_fields(data, USER_FIELDS):
+        response = {
+            'error': True,
+            'status_code': 400,
+            'message': 'Invalid or missing fields in request.'
+        }
+        return jsonify(response), 400
+
+    insert_user(data)
+
+@version.route("/signup/tutor", methods=["POST"])
+def signup_tutor():
+    data = request.get_json()
+
+    if not validate_fields(data, TUTOR_FIELDS):
+        response = {
+            'error': True,
+            'status_code': 400,
+            'message': 'Invalid or missing fields in request.'
+        }
+        return jsonify(response), 400
+
+    # If validation passes, you can continue with inserting the data into the database.
+    insert_tutor(data)
+
+
+
+#####################
+# Main
+#####################
 
 if __name__ == "__main__":
+    app.register_blueprint(version)
     app.run(debug=True, host="0.0.0.0")
+    
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+
+# with app.app_context():
+#     connection = db.engine.connect()
+#     try:
+#         result = connection.execute(text("SELECT * FROM users"))
+#         print("Connection successful:", result.fetchone())
+#     except Exception as e:
+#         print("Error:", e)
+#     finally:
+#         connection.close()
