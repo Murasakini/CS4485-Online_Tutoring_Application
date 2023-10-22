93f759fb3e2b13e4509488ae78edcea014079f2e
kcao2 <104512360+kcao2@users.noreply.github.com>
2023-10-22 16:58:59 -0500

    Update SignIn.js

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Navigate } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -11,8 +10,9 @@ import Box from '@mui/material/Box';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css';
 import { useState } from 'react';
-//import axios from 'axios';
-import FrontAPI from './api/FrontAPI.js';
+import FrontAPI from './FrontAPI.js';
+import { setSessionCookie } from './CookieUtils';
+import Copyright from './Copyright';
 
 const theme = createTheme({
   palette: {
@@ -25,20 +25,7 @@ const theme = createTheme({
   },
 });
 
-export function Copyright() {
-  return (
-    <h4>
-      Copyright © 
-      <Link color="inherit" href="https://www.utdallas.edu/">
-        The University of Texas at Dallas
-      </Link>
-    </h4>
-  );
-}
-
 export default function SignIn() {
-  // keep track if logging in successfully
-  const [isSuccessful, setSuccessful] = useState(false);
 
   //form as a state
   const [formData, setFormData] = useState({
@@ -57,118 +44,107 @@ export default function SignIn() {
 
   //listen for submit event from "Sign in" Button
   const handleSubmit = async (event) => {
-    //preventDefault() prevents a page refresh 
     event.preventDefault();
-    
     try {
+      // response would be token
       const response = await FrontAPI.signIn(formData);
-  
-      if (response.status_code === 200) {
-        // success msg
-        console.log('Login successful');
+      // TODO: need to validate data structure of response fron FrontAPI (response.data.token or what?)
+      setSessionCookie('authToken', response.token, { 
+        expires: 1/24,       // 1 hour expiration
+        secure: true,        // only transmitted over HTTPS (encrypted)
+        httpOnly: true,      // inaccessible to JavaScript (client cannot modify cookie)
+        sameSite: 'strict'   // prevents CSRF (cookie can only be sent by site that set cookie)
+      });
 
-        // set sign up status as successful
-        setSuccessful(true);
-      } else {
-        // fail msg
-        console.log('Login failed');
-      }
-    } 
-    catch (error) {
-      // error msg
+      // success. add logic here
+      console.log('Sign in was successful');
+    } catch (error) {
+      // error from FrontAPI
       console.error('Error:', error);
     }
   };
   
   return (
-    <React.Fragment>
-      {isSuccessful ? 
-        // navigate to home page
-        <Navigate to="/" /> :
+      <ThemeProvider theme={theme}>
+          <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}> 
+            <Link href="https://www.utdallas.edu/">
+              <Avatar sx={{ bgcolor: 'primary.main', width: 60, height: 60 }}>
+                <h4>UTD</h4>
+              </Avatar>
+            </Link>
+          </Box>
 
-        // display input form
-        <ThemeProvider theme={theme}>
-            <Box sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                }}> 
-              <Link href="https://www.utdallas.edu/">
-                <Avatar sx={{ bgcolor: 'primary.main', width: 60, height: 60 }}>
-                  <h4>UTD</h4>
-                </Avatar>
-              </Link>
-            </Box>
+          <h2>Online Tutoring Service</h2>
+          <h2>SIGN IN</h2>
 
-            <h2>Online Tutoring Service</h2>
-            <h2>SIGN IN</h2>
+          <Box component="form" onSubmit={handleSubmit} >
 
-            <Box component="form" onSubmit={handleSubmit} noValidate >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
 
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                type="email"
-                value={formData.email}
-                onChange={handleChange}
-              />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            
+            <FormControlLabel
+              control={<Checkbox value="remember"/>}
+              label="Remember me"
+            />
 
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={formData.password}
-                onChange={handleChange}
-              />
-              
-              <FormControlLabel
-                control={<Checkbox value="remember"/>}
-                label="Remember me"
-              />
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 2, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              
-              {/* hyper links */}
-              <Grid container justifyContent="space-between">
-                <Grid item>
-                  <Link href="#" variant="body1">
-                    Forgot your password?
-                  </Link>
-                </Grid>
-
-                <Grid item>
-                  <Link href="#" variant="body1">
-                    Don't have an account? Sign Up
-                  </Link>
-                </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            
+            {/* hyper links */}
+            <Grid container justifyContent="space-between">
+              <Grid item>
+                <Link href="#" variant="body1">
+                  Forgot your password?
+                </Link>
               </Grid>
 
-            </Box>
-
-            <Grid item sx={{mt: 10}}>
-              <Copyright/>
+              <Grid item>
+                <Link href="#" variant="body1">
+                  Don't have an account? Sign Up
+                </Link>
+              </Grid>
             </Grid>
 
-        </ThemeProvider>
-      }
-    </React.Fragment>
+          </Box>
+
+          <Grid item sx={{mt: 10}}>
+            <Copyright/>
+          </Grid>
+
+      </ThemeProvider>
   );
 }
