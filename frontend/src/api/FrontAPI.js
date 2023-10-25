123295f87640d0a25757a22a8da7b3fa7c284fa8
snopiro <khoochris13@gmail.com>
2023-10-25 18:40:00 -0500

    new link

--- frontend/src/api/FrontAPI.js
+++ frontend/src/api/FrontAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { SHA256 } from 'crypto-js';
 
-const baseURL = 'https://b69e-64-189-201-30.ngrok-free.app';
+const baseURL = 'https://3a40-64-189-200-251.ngrok-free.app';
 
 const axiosInstance = axios.create({
   baseURL,
