e94bac51f75d233048bc4ec01e0b888a65f1c0e6
kcao2 <104512360+kcao2@users.noreply.github.com>
2023-11-16 19:51:12 -0600

    Update Utils.js | added convertToMySQLTimestamp

--- frontend/src/Utils.js
+++ frontend/src/Utils.js
@@ -30,7 +30,19 @@ export const calculateAvailableTimeSlots = (allPossibleTimeSlots, tutorTimeSlots
   const availableSlots = allPossibleTimeSlots.filter(
     (slot) => !bookedTimeSlots.has(new Date(slot).getTime())
   );
+  console.log('time', availableSlots);
   return availableSlots;
 };
 
+export const convertToMySQLTimestamp = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
 
+  const mysqlTimestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+  return mysqlTimestamp;
+};
